9c3134d08b6d9dd80e63c263c7e39576d17c9c19
GodDamnGeek <kirankumarkk.oxford@gmail.com>
2018-05-29 17:21:27 +0530

    minor corrections

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -12,8 +12,8 @@ from flask_login import login_user,logout_user,current_user,login_required
 @app.route("/")
 @app.route("/home")
 def home():
-	post = request.args.get('page',1,type=int)
-	posts = Post.query.paginate(page=page, per_page=5)
+	page = request.args.get('page',1,type=int)
+	posts = Post.query.paginate(page=page, per_page=2)
 	return render_template('home.html',posts=posts)
 
 
