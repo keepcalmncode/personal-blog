9e18055231ae67782c73a0c727905eefa96b312c
GodDamnGeek <kirankumarkk.oxford@gmail.com>
2018-06-03 12:26:52 +0530

    added mail init.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -12,5 +13,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 547
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'keepcalmncode@gmail.com'
+app.config['MAIL_PASSWORD'] = '9243997942'
+mail = Mail(app)
+
 
 from flaskblog import routes
